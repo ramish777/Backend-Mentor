eeed876637cb77e8c6f255e4d9340428c6c43407
Ramish Afaq Bhutta <130045967+ramish777@users.noreply.github.com>
2024-04-11 18:03:03 +0500

    made landing page

---
+++ src/components/landingPage.component.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import '../css/landingPage.css';
+
+export default class landingPage extends Component {
+    render() {
+        return (
+            <div className="landing-page">
+                <div className="content">
+                    <div className="button-container">
+                        <button className="gradient-button">Helping people learn backend with ease</button>
+                    </div>
+                    <h1>Build Backends, Learn Concepts</h1>
+                    <h1>& Become the Expert</h1>
+                    <p>Build Backends, Learn Concepts & Become the Expert</p>
+                    <div className="frame14">
+                        <button className="outline-button">Become the Expert</button>
+                        <button className="outline-button view-challenges-button">
+                            View Challenges
+                        </button>
+                        <div className="challenge-details">
+                                <div>50+ challenges</div>
+                                <div>for your skills</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
